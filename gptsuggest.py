1f66e0abbe10f27fc629c12558c9614b76471422
Louis Lai <louis@aukilabs.com>
2023-10-26 00:16:06 +0800

    try perpective

--- gptsuggest.py
+++ gptsuggest.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 from typing import List, Tuple
 
-def detect_rotated_qr_code(image_path: str) -> List[Tuple]:
+def detect_qr_code_with_cone_effect(image_path: str) -> List[Tuple]:
     """
-    Detect a QR code in an image that may be rotated. The image is rotated degree by degree for 360 times.
+    Simulate the effect of viewing a QR code from different angles around a cone and detect it.
     
     Parameters:
     - image_path (str): The path to the image file where the QR code is to be detected.
@@ -13,9 +13,9 @@ def detect_rotated_qr_code(image_path: str) -> List[Tuple]:
     - List of detected QR codes information (List[Tuple]).
       Each tuple contains (decoded_info, points, angle)
     """
-    
     # Read the input image
     img = cv2.imread(image_path)
+    h, w = img.shape[:2]
     
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -29,12 +29,20 @@ def detect_rotated_qr_code(image_path: str) -> List[Tuple]:
     detected_qrs = []
 
     for angle in range(0, 360, 10):
-        # Rotate the image
-        M = cv2.getRotationMatrix2D((gray.shape[1] // 2, gray.shape[0] // 2), angle, 1)
-        rotated_gray = cv2.warpAffine(gray, M, (gray.shape[1], gray.shape[0]))
+        # Simulate the perspective effect due to the cone
+        pts1 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
+        offset = 50 * np.sin(np.radians(angle))  # simulate cone effect
+        pts2 = np.float32([[0+offset, 0+offset], [w-offset, 0+offset], [0+offset, h-offset], [w-offset, h-offset]])
+
+        M_perspective = cv2.getPerspectiveTransform(pts1, pts2)
+        perspective_img = cv2.warpPerspective(gray, M_perspective, (w, h))
         
+        cv2.imshow(f"Detected QR codes at {angle} degrees", perspective_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
         # Detect and decode the QR code
-        detection_success, decoded_info, points, _ = detector.detectAndDecodeMulti(rotated_gray)
+        detection_success, decoded_info, points, _ = detector.detectAndDecodeMulti(perspective_img)
         
         if detection_success:
             print(f"QR code(s) detected at {angle} degrees!")
@@ -42,14 +50,13 @@ def detect_rotated_qr_code(image_path: str) -> List[Tuple]:
                 print(f"Decoded Info {i+1}: {decoded_info[i]}")
                 pts = np.array(points[i], dtype=np.int32)
                 pts = pts.reshape((-1, 1, 2))
-                cv2.polylines(rotated_gray, [pts], True, (0, 255, 0), 2)
+                cv2.polylines(perspective_img, [pts], True, (0, 255, 0), 2)
                 detected_qrs.append((decoded_info[i], points[i], angle))
 
             # Uncomment the following lines if you want to display the rotated image where QR code is detected
-            cv2.imshow(f"Detected QR codes at {angle} degrees", rotated_gray)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-            return detected_qrs
+            # cv2.imshow(f"Detected QR codes at {angle} degrees", perspective_img)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
     
     if not detected_qrs:
         print("No QR code detected in any rotated images.")
@@ -58,6 +65,5 @@ def detect_rotated_qr_code(image_path: str) -> List[Tuple]:
 
 # Test the function
 if __name__ == "__main__":
-    detected_qrs = detect_rotated_qr_code('test_photos/bak/IMG_0169b.jpg')
-    # detected_qrs = detect_rotated_qr_code('3_debug copy.jpg')
+    detected_qrs = detect_qr_code_with_cone_effect('test_photos/bak/IMG_0169b.jpg')
     print(f"Detected QR codes: {detected_qrs}")
