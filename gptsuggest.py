f15d5ffb0a3e725e42649934fc9bb15ab01fac8c
Louis Lai <louis@aukilabs.com>
2023-10-25 23:47:55 +0800

    test detector.detectAndDecodeMulti

--- gptsuggest.py
+++ gptsuggest.py
@@ -3,6 +3,17 @@ import numpy as np
 from typing import List, Tuple
 
 def detect_qr_code(image_path: str) -> List[Tuple]:
+    """
+    Detect a QR code in an image, which may be rotated, distorted, or colored differently.
+    
+    Parameters:
+    - image_path (str): The path to the image file where the QR code is to be detected.
+    
+    Outputs:
+    - List of detected QR codes information (List[Tuple]).
+      Each tuple contains (decoded_info, points)
+    """
+    
     # Read the input image
     img = cv2.imread(image_path)
     
@@ -14,10 +25,31 @@ def detect_qr_code(image_path: str) -> List[Tuple]:
     
     # Use the OpenCV QRCodeDetector
     detector = cv2.QRCodeDetector()
-
-    return detector.detectAndDecodeMulti(gray)
+    
+    # Detect and decode the QR code
+    detection_success, decoded_info, points, straight_qrcode = detector.detectAndDecodeMulti(gray)
+    
+    detected_qrs = []
+    
+    if detection_success:
+        print("QR code(s) detected!")
+        for i in range(len(decoded_info)):
+            print(f"Decoded Info {i+1}: {decoded_info[i]}")
+            pts = np.array(points[i], dtype=np.int32)
+            pts = pts.reshape((-1, 1, 2))
+            cv2.polylines(img, [pts], True, (0, 255, 0), 2)
+            detected_qrs.append((decoded_info[i], points[i]))
+        
+        # Display the output image with detected QR codes
+        cv2.imshow("Detected QR codes", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print("No QR code detected.")
+    
+    return detected_qrs
 
 # Test the function
 if __name__ == "__main__":
-    detected_qrs = detect_qr_code('qr_code_test.png')
+    detected_qrs = detect_qr_code('test_photos/bak/IMG_2340b.jpg')
     print(f"Detected QR codes: {detected_qrs}")
